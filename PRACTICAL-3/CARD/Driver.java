8ade9fb763a61fdeceed04feb6bfe3eed20c12f0
AARAV PATEL <patelaarav1512@gmail.com>
2026-08-11 18:15:20 +0530

    Update

--- PRACTICAL-3/CARD/Driver.java
+++ PRACTICAL-3/CARD/Driver.java
@@ -1,3 +1,23 @@
 public class Driver {
-    
+    public static void main(String[] args) {
+        Card[] cards = new Card[5];
+
+        // Add cards one at a time
+        cards[0] = new Card("Ace", "Spades");
+        cards[1] = new Card("Queen", "Hearts");
+        cards[2] = new Card("King", "Diamonds");
+        cards[3] = new Card("Ace", "Spades"); // duplicate
+        cards[4] = new Card("Jack", "Clubs");
+
+        // Check for duplicates as we add
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (cards[i].equals(cards[j])) {
+                    System.out.println("Duplicate found: " + cards[i]);
+                    // Stop after first duplicate found
+                    return;
+                }
+            }
+        }
+    }
 }
--- PRACTICAL-3/POINT/Driver.java
+++ PRACTICAL-3/POINT/Driver.java
@@ -1,3 +1,30 @@
 public class Driver {
-    
+    public static void main(String[] args) {
+        // Create array with repeated points
+        Point[] points = {
+            new Point(1, 2),
+            new Point(3, 4),
+            new Point(1, 2), // repeat
+            new Point(5, 6),
+            new Point(3, 4), // repeat
+        };
+
+        int distinctCount = 0;
+
+        // Check distinct points manually
+        for (int i = 0; i < points.length; i++) {
+            boolean seenBefore = false;
+            for (int j = 0; j < i; j++) {
+                if (points[i].equals(points[j])) {
+                    seenBefore = true;
+                    break;
+                }
+            }
+            if (!seenBefore) {
+                distinctCount++;
+            }
+        }
+
+        System.out.println("Distinct: " + distinctCount);
+    }
 }
